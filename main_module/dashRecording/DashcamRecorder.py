ce6715552d06e1aa5768b4fc2f78f3cdad9bc379
Henry Bergin <tackyattack@gmail.com>
2019-10-08 16:14:15 -0500

    Organization and file add

---
+++ main_module/dashRecording/DashcamRecorder.py
@@ -0,0 +1,6 @@
+class Recorder:
+    pass
+    # use terminate for thread joining
+    # specify record folder
+    # Wrap files into mp4 -- don't transcode
+    # two threads -- one for folder management and wrapping, other for stopping/starting at 60sec
