0f85bcb260cc3bf5951a541f34278122c7c43099
Daniel Kraus <djkyr4@umsystem.edu>
2019-10-17 14:54:55 -0500

    changed layout and functions
    changed layout and functions, ready to be implemented with the rest of the project

--- main_module/gui.py
+++ main_module/gui.py
@@ -1,32 +1,34 @@
-import PySimpleGUI27 as sg
+import sys
+if sys.version_info[0] >= 3:
+    import PySimpleGUI as sg
+else:
+    import PySimpleGUI27 as sg
+
+
+sg.ChangeLookAndFeel('Dark')
+sg.SetOptions(element_padding=(1,1))
 
 def start_gui():
-    sg.ChangeLookAndFeel('Dark')
-    sg.SetOptions(element_padding=(4, 4))
 
-    layout = [
+layout = [
 
-        [sg.Button('Rear Camera', button_color=('white', 'red'), size=(20, 5), key='Rear Camera'),
-         sg.Button('Function1', button_color=('white', 'blue'), size=(20, 5), key='Function1'),
-         sg.Button('Function2', button_color=('white', 'green'), size=(20, 5), key='Function2'),
-         sg.Button('Function3', button_color=('white', 'yellow'), size=(20, 5), key='Function3')]
-        ]
 
-    window = sg.Window('Smart Dashcam GUI', layout, no_titlebar=True, location=(0,0), size=(480,320), keep_on_top=True)
+      [sg.Button('Rear Camera', button_color=('white', 'red'), size=(20, 5), key='Rear Camera'),
+      sg.Button('Other', button_color=('white', 'blue'), size=(20, 5), key='Other')],
+      [sg.FilesBrowse('Browse Recordings', button_color=('white', 'green'), size=(20, 5), key='Browse'),
+      sg.Button('Exit', button_color=('black', 'yellow'), size=(20, 5), key='Exit')],
 
-    while True:             # Event Loop
+
+    ]
+
+window = sg.Window('Smart Dashcam GUI', layout, no_titlebar=True, location=(0,0), size=(480,320), keep_on_top=True)
+
+while True:  # Event Loop
         event, values = window.Read()
         if event in (None, 'Exit'):
             break
         if event == 'Rear Camera':
             func('Rear Camera')
-        elif event == 'Function 1':
-            func('Function 1')
-        elif event == 'Function 2':
-            func('Function 2')
-        elif event == 'Function 3':
-            func('Function 3')
-    window.Close()
-
-    event, values = window.Read()
-    text_input = values[0]
+        elif event == 'Other':
+            func('Other')
+window.Close()
