6b7e95dbe0d8fa1a5b271a3107538ae582c134b1
Mike Mikitin <mpm729@umsystem.edu>
2019-10-31 17:28:39 -0500

    fixed pizero install and config file - samba

--- NetworkShare/Samba/sambapiZeroBash.sh
+++ NetworkShare/Samba/sambapiZeroBash.sh
@@ -1,21 +1,14 @@
 # Install Samba
 sudo apt-get update
-sudo apt-get install samba
-
-# Start Samba
-sudo service smbd start
+sudo apt-get install samba samba-client samba-common
 
 # Make backup copy of the config file -> named smb_bkp.conf
-sudo cp /etc/samba/smb.conf /etc/samba/smb_bkp.conf
-
-# Remove original smb.conf
-sudo rm /etc/samba/smb.conf
+sudo cp -f /etc/samba/smb.conf /etc/samba/smb_bkp.conf
 
 # Bring in updated conf file
-sudo cp samba_config_for_piZero.conf /etc/samba/smb.conf
+sudo cp -f samba_config_for_piZero.conf /etc/samba/smb.conf
 
-# Mount Shared piZero Drive
-sudo mkdir recordings
-sudo mount -t cifs //192.168.200.2/sambashare ~/recordings
+# Start Samba
+sudo service smbd restart
 
 echo "Done with Samba Config and Mounting"
