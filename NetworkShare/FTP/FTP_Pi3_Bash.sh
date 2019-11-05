c338332225e68388c0b2b4de2c9e05748703993f
Mike Mikitin <mpm729@umsystem.edu>
2019-11-05 16:00:41 -0600

    install bash updated

--- NetworkShare/FTP/FTP_Pi3_Bash.sh
+++ NetworkShare/FTP/FTP_Pi3_Bash.sh
@@ -2,19 +2,24 @@
 sudo apt-get update
 sudo apt-get install proftpd
 
-# Start proftpd
-sudo service proftpd start
+sudo mkdir -p Backups
 
-# Make backup copy of the config file -> named smb_bkp.conf
-sudo cp /etc/proftpd/proftpd.conf /etc/proftpd/proftpd_bkup.conf
 
-# Remove original proftpd.conf
-sudo rm /etc/proftpd/proftpd.conf
+# Make backup copy of the config file -> named proftpd_bkp.conf
+
+if [ -f "Backups/proftpd_bkp.conf" ]
+then
+    echo "Backup proftpd config already exists!"
+else
+    sudo cp -f /etc/proftpd/proftpd.conf Backups/proftpd_bkp.conf
+fi
 
 # Bring in updated conf file
 sudo cp FTPConfigPi3.conf /etc/proftpd/proftpd.conf
 
-
 sudo service proftpd reload
+
+# Start proftpd
+sudo service proftpd start
 
 echo "Done with FTP Config"
